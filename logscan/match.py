010ce68060c30c00783b3ebf5ad1c9b83a4ec2ff
comyn <comyn@localhost.localdomain>
2016-03-19 10:44:35 +0800

    change match

--- logscan/match.py
+++ logscan/match.py
@@ -1,111 +1,114 @@
-import re
-
-fn_map = {
-    '&': lambda x, y:  x and y,
-    '|': lambda x, y: x or y,
-    '!': lambda x: not x
-}
-
+from queue import Queue
 
 class Token:
-    LEFT_BRACKETS = 1
-    RIGHT_BRACKETS = 2
-    SYMBOL = 3
-    EXPRESSION = 4
-
+    LEFT_BRACKETS = 'LEFT_BRACKETS'
+    RIGHT_BRACKETS = 'RIGHT_BRACKETS'
+    SYMBOL = 'SYMBOL'
+    EXPRESSION = 'EXPRESSION'
     SYMBOLS = '&|!'
 
     def __init__(self, value, type):
         self.value = value
         self.type = type
 
+    def __str__(self):
+        return '{0}<{1}>'.format(self.value, self.type)
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class ASTree:
-    def __init__(self, value):
-        self.root = value
+    def __init__(self, token):
+        self.root = token
         self.left = None
         self.right = None
 
+    def visit(self):
+        ret = []
+        q = Queue()
+        q.put(self)
+        while not q.empty():
+            t = q.get()
+            ret.append(t.root)
+            if t.left:
+                q.put(t.left)
+            if t.right:
+                q.put(t.right)
+        return ret
 
-class Matcher:
-    def __init__(self, origin):
-        self.origin = origin
-        self.ast = Matcher.make_ast(self.tokenize())
 
-    def tokenize(self):
-        tokens = []
-        is_expr = False
-        expr = []
-        for c in self.origin:
-            if c == '#':
-                if not is_expr:
-                    is_expr = True
-                else:
-                    is_expr = False
-                    tokens.append(Token(''.join(expr), Token.EXPRESSION))
-                    expr = []
-            elif is_expr:
-                expr.append(c)
-            elif c in Token.SYMBOLS:
-                tokens.append(Token(c, Token.SYMBOL))
-            elif c == '(':
-                tokens.append(Token(c, Token.LEFT_BRACKETS))
-            elif c == ')':
-                tokens.append(Token(c, Token.RIGHT_BRACKETS))
-        return tokens
-
-    @staticmethod
-    def make_sub_tree(stack, t):
-        current = t
-        while stack and stack[-1].root.type != Token.LEFT_BRACKETS:
-            node = stack.pop()
-            if node.root.type != Token.SYMBOL:
-                raise Exception("parse error")
-            node.right = current
-            if node.root.value == '&' or node.root.value == '|':
-                left = stack.pop()
-                if left.root.type != Token.SYMBOL and left.root.type != Token.EXPRESSION:
-                    raise Exception("parse error")
-                node.left = left
-            current = node
-        stack.append(current)
-
-    @staticmethod
-    def make_ast(tokens):
-        stack = []
-        for token in tokens:
-            t = ASTree(token)
-            if t.root.type == Token.SYMBOL or t.root.type == Token.LEFT_BRACKETS:
-                stack.append(t)
-            elif t.root.type == Token.EXPRESSION:
-                Matcher.make_sub_tree(stack, t)
+def tokenize(origin):
+    tokens = []
+    is_expr = False
+    expr = []
+    for c in origin:
+        if c == '#':
+            if not is_expr:
+                is_expr = True
             else:
-                t = stack.pop()
-                tmp = stack.pop()
-                if tmp.root.type != Token.LEFT_BRACKETS:
-                    raise Exception("parse error")
-                Matcher.make_sub_tree(stack, t)
-        return stack.pop()
+                is_expr = False
+                token = Token(''.join(expr), Token.EXPRESSION)
+                tokens.append(token)
+                expr = []
+        elif is_expr:
+            expr.append(c)
+        elif c in Token.SYMBOLS:
+            token = Token(c, Token.SYMBOL)
+            tokens.append(token)
+        elif c == '(':
+            token = Token(c, Token.LEFT_BRACKETS)
+            tokens.append(token)
+        elif c == ')':
+            token = Token(c, Token.RIGHT_BRACKETS)
+            tokens.append(token)
 
-    @staticmethod
-    def eval(ast, line):
+    return tokens
 
-        if ast.root.type == Token.EXPRESSION:
-            return re.match(ast.root.value, line) is not None
-        if ast.root.value == '!':
-            if ast.right is None:
-                raise Exception('syntax error')
-            return fn_map['!'](Matcher.eval(ast.right, line))
-        if ast.left is None:
-            raise Exception('syntax error')
-        if ast.right is None:
-            raise Exception('syntax error')
-        return fn_map[ast.root.value](Matcher.eval(ast.left, line), Matcher.eval(ast.right, line))
 
-    def match(self, line):
-        return Matcher.eval(self.ast, line)
+def make_sub_ast(stack, t):
+    current = t
+    while stack and stack[-1].root.type != Token.LEFT_BRACKETS:
+        node = stack.pop()
+        if node.root.type != Token.SYMBOL:
+            raise Exception('parse error, excepted {0} but {1}'.format(Token.SYMBOL, node.root))
+        node.right = current
+        if node.root.value == '&' or node.root.value == '|':
+            left = stack.pop()
+            if left.root.type != Token.SYMBOL and left.root.type != Token.EXPRESSION:
+                raise Exception('parse error, excepted {0} or {1} but {2}'.format(Token.SYMBOL,
+                                                                                  Token.EXPRESSION, left.root))
+            node.left = left
+        current = node
+    stack.append(current)
+
+
+def make_ast(tokens):
+    stack = []
+    for t in tokens:
+        tree = ASTree(t)
+        if tree.root.type == Token.SYMBOL or tree.root.type == Token.LEFT_BRACKETS:
+            stack.append(tree)
+        elif tree.root.type == Token.EXPRESSION:
+            make_sub_ast(stack, tree)
+        else:
+            sub_tree = stack.pop()
+            if sub_tree.root.type != Token.SYMBOL and sub_tree.root.type != Token.EXPRESSION:
+                raise Exception('parse error, excepted {0} or {1} but {2}'.format(Token.SYMBOL,
+                                                                                  Token.EXPRESSION, sub_tree))
+            tmp = stack.pop()
+            if tmp.root.type != Token.LEFT_BRACKETS:
+                raise Exception('paser error, excepted {0} but {1}'.format(Token.LEFT_BRACKETS, tmp))
+            make_sub_ast(stack, sub_tree)
+    return stack.pop()
 
 
 if __name__ == '__main__':
-    m = Matcher('#test# & #abc# | (!#sdf# & #123#)')
-    print(m.match("test acd dds 234"))
+    e = '#test# & #abc# |(!#123# | #456#)'
+    #print([str(x) for x in tokenize(e)])
+    # for t in tokenize(e):
+    #     print(t)
+
+    ast = make_ast(tokenize(e))
+    print(ast.visit())
+
